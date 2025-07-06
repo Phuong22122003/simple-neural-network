7e469612ef83b14fec79141cd67d72aa9b97bfdb
Phuong221203 <hnguyenphuong09@gmail.com>
2025-07-06 14:44:22 +0700

    rename backward input

--- layers/base.py
+++ layers/base.py
@@ -2,6 +2,6 @@ class Layer:
     def __init__(self):
         pass
     def forward(self,inputs):
-        pass
-    def backward(self,dl_da):
-        pass
+        raise NotImplementedError
+    def backward(self,grad_outputs):
+        raise NotImplementedError
